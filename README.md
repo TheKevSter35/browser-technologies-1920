0b3fd42e8c7a738ac8d4c52048dab697f7a96ead
TheKevSter-NL <43183768+TheKevSter-NL@users.noreply.github.com>
2020-03-18 17:48:13 +0100

    Starting writing  a HTML page

--- README.md
+++
@@ -1,3 +0,0 @@
-## See assignments 
-
-https://github.com/TheKevSter35/browser-technologies-1920/wiki
---
+++ js/app.js
@@ -0,0 +1,12 @@
+import { api, apiUserinput} from './modules/api.js';
+
+
+
+
+let button = document.getElementById('submitbtn')
+button.addEventListener("click", apiUserinput);
+
+
+api()
+
+
