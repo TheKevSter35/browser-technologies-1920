6ce013ddd28d5914a02202c089e2a2cccd4def3e
TheKevSter-NL <43183768+TheKevSter-NL@users.noreply.github.com>
2020-03-19 15:57:26 +0100

    use npm with express and ejs, Added Style

---
+++ app.js
@@ -0,0 +1,32 @@
+let express = require('express')
+let bodyParser = require('body-parser');
+let path = require('path');
+let fetch = require('node-fetch');
+let app = express()
+let port = process.env.PORT || 3000
+
+//view Engines 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Set Static path for non html code like pictures and CSS
+app.use(express.static(path.join(__dirname + 'public')));
+
+
+app.get('/', (req, res) => {
+    res.render('pages/index')
+  })
+
+  
+
+// app.get('/', (req, res) => {
+//     res.render('pages/index')
+//   })
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ public/js/script.js
@@ -0,0 +1,76 @@
+//Font
+
+let Defaultfont = document.getElementById('Default-font')
+Defaultfont.addEventListener("click", ChangeDefaultfont);
+
+function ChangeDefaultfont() {
+    document.getElementById("textcanvas").style.fontFamily = ('Raleway')
+}
+let Streamster = document.getElementById('Streamstar-font')
+Streamster.addEventListener("click", ChangeStreamsterfont);
+
+function ChangeStreamsterfont() {
+    document.getElementById("textcanvas").style.fontFamily = ('Streamster')
+
+}
+let Learningblocks = document.getElementById('Learningblocks-font')
+Learningblocks.addEventListener("click", ChangeLearningblocksfont);
+
+function ChangeLearningblocksfont() {
+    document.getElementById("textcanvas").style.fontFamily = ('Nixies')
+
+}
+
+//text input
+let textInput = document.getElementById('textInputButton')
+textInput.addEventListener("click", Shirttext);
+
+function Shirttext() {
+    let textValue = document.getElementById('Shirttext').value
+    document.querySelector("h2").innerHTML=textValue
+
+}
+//text Color
+
+let Blacktextcolor = document.getElementById('Black-textcolor')
+Blacktextcolor.addEventListener("click", ChangeColorBlack);
+
+function ChangeColorBlack() {
+    document.getElementById("textcanvas").style.color = ('black')
+
+}
+let Whitetextcolor = document.getElementById('White-textcolor')
+Whitetextcolor.addEventListener("click", ChangeColorWhite);
+
+function ChangeColorWhite() {
+    document.getElementById("textcanvas").style.color = ('white')
+
+}
+
+
+//Shirt color
+let White = document.getElementById('White-shirtcolor')
+White.addEventListener("click", ShirtWhite);
+
+function ShirtWhite() {
+    document.getElementById("shirtcanvas").src = "../source/pictures/shirts/white.jpg"   
+}
+
+let Yellow = document.getElementById('Yellow-shirtcolor')
+Yellow.addEventListener("click", ShirtYellow);
+
+function ShirtYellow() {
+    document.getElementById("shirtcanvas").src = "../source/pictures/shirts/yellowshirt.jpg"   
+}
+let Red = document.getElementById('Red-shirtcolor')
+Red.addEventListener("click", ShirtRed);
+
+function ShirtRed() {
+    document.getElementById("shirtcanvas").src = "../source/pictures/shirts/redshirt.jpg" 
+}
+let Blue = document.getElementById('Blue-shirtcolor')
+Blue.addEventListener("click", ShirtBlue);
+
+function ShirtBlue() {
+    document.getElementById("shirtcanvas").src = "../source/pictures/shirts/blueshirt.jpg"
+}
